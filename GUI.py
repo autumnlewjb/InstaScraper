ad6f64b9059b41a2bdf8bb2359a2914093180f15
Lew <autumnlewjb@gmail.com>
2020-06-03 17:39:36 +0800

    figure out the stringvar

--- GUI.py
+++ GUI.py
@@ -1,20 +1,30 @@
 from tkinter import *
 
-username = None
 email = None
+username = None
 password = None
 
 
-def get_value(entry1, entry2, entry3):
-    global username, email, password
+class Gui(Tk):
+    def close_and_open(self):
+        self.destroy()
+        get_info()
 
-
-
-    print(email, password, username)
+    def close_and_print(self):
+        global email, username, password
+        self.destroy()
+        email = email.get()
+        username = username.get()
+        password = password.get()
+        print(email, username, password)
 
 
 def get_info():
-    root2 = Tk()
+    global email, username, password
+    root2 = Gui()
+    email = StringVar()
+    username = StringVar()
+    password = StringVar()
 
     master_frame_1 = Frame(master=root2)
     master_frame_2 = Frame(master=root2)
@@ -34,60 +44,34 @@ def get_info():
     f2 = Frame(master=master_frame_1)
     f2.pack()
 
-    entry1 = Entry(master=f2, borderwidth=5, relief=GROOVE)
-    entry2 = Entry(master=f2, borderwidth=5, relief=GROOVE, show='*')
-    entry3 = Entry(master=f2, borderwidth=5, relief=GROOVE)
+    # global username, email, password
+
+    entry1 = Entry(master=f2, borderwidth=5, relief=GROOVE, textvariable=email)
+    entry2 = Entry(master=f2, borderwidth=5, relief=GROOVE, show='*', textvariable=password)
+    entry3 = Entry(master=f2, borderwidth=5, relief=GROOVE, textvariable=username)
 
     entry1.pack()
     entry2.pack()
     entry3.pack()
 
-    global username, email, password
-
     f3 = Frame(master=master_frame_2)
     f3.pack()
-    confirm = Button(master=f3, text='Confirm', )
+    confirm = Button(master=f3, text='Confirm', command=root2.destroy)
     confirm.pack()
 
-    email = entry1.get()
-    password = entry2.get()
-    username = entry3.get()
-
-    print(username, password, email)
-
     root2.mainloop()
+    email = email.get()
+    username = username.get()
+    password = password.get()
+    print(email, username, password)
 
 
-root = Tk()
+root = Gui()
 root.title('Get Started')
 
 label = Label(text='Get Started')
 label.pack()
 
-button = Button(text='OK', command=get_info)
+button = Button(text='OK', command=root.close_and_open)
 button.pack()
-
-# frame_a = Frame(master=root, width=50, height=50, bg='red')
-# frame_a.pack(side=LEFT)
-# # label_a = Label(master=frame_a, text='frame 1')
-# # label_a.pack()
-#
-# frame_b = Frame(master=root, bg='blue')
-# frame_b.pack(side=LEFT)
-# label_b = Label(master=frame_b, text='frame 2')
-# label_b.pack()
-
-# main = Frame(root, height=1, width=0.5)
-# main.place(relx=0.1, rely=0.1, relwidth=0.1, relheight=0.1)
-# main.pack(anchor=CENTER)
-#
-# text = Text(main, height=1, width=30)
-#
-# text.tag_config('center', justify='center')
-# text.insert(INSERT, 'Wanna Scrape from Instagram?')
-# text.pack(anchor=CENTER)
-#
-# button = Button(root, text='OK', justify='center')
-# button.pack(anchor=S)
-
 root.mainloop()
